720c5f5d170c93cf3059c7982d45a675830837ab
YJ981100 <93577657+YJ981100@users.noreply.github.com>
2023-10-23 13:48:38 +0200

    DVS check

--- run-consumer.py
+++ run-consumer.py
@@ -88,14 +88,14 @@ def write_row_to_grids(row_col_data, row, ncols, header, path_to_output_dir, pat
     
     output_grids_ic = {
         "1": {
-            "Yield": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 1},
-            "AbBiom": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 1},
-            "TraDef": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 2},
-            "NDef": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 2},
-            "DOY|SowDOY": {"data" : make_dict_nparr(), "cast-to": "int"},
-            "DOY|AntDOY": {"data" : make_dict_nparr(), "cast-to": "int"},
-            "DOY|MatDOY": {"data" : make_dict_nparr(), "cast-to": "int"},
-            "LightInterception1": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 2},
+            #"Yield": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 1},
+            #"AbBiom": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 1},
+            #"TraDef": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 2},
+            #"NDef": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 2},
+            "SowDOY": {"data" : make_dict_nparr(), "cast-to": "int"},
+            "AntDOY": {"data" : make_dict_nparr(), "cast-to": "int"},
+            "HarDOY": {"data" : make_dict_nparr(), "cast-to": "int"}
+            #"LightInterception1": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 2},
             #"Mois|SoilAvW": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 2},
             #"TraDef|Gendrou": {"data" : make_dict_nparr(), "cast-to": "float", "digits": 2},
             # define Gendrou as the median of transpiration deficit during generative phase
